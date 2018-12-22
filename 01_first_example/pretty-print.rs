f17c3a0115a6b3b425f31dfd90761396998ff9e4
Abhijith Nagarajan <abhijith.nagarajan@servicenow.com>
2018-12-22 07:28:15 +0100

    Debug & Pretty Print

---
+++ 01_first_example/pretty-print.rs
@@ -0,0 +1,13 @@
+#[derive(Debug)]
+struct Person<'a> {
+	name: &'a str,
+	age: u8
+}
+
+fn main() {
+	let name = "Peter";
+	let age = 28;
+	let person = Person {name, age};
+
+	println!("{:#?}", person);
+}
